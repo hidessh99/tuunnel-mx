9a4b5509faa0438619ebd1b0feb4b68ce59c5fa7
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-03 18:48:13 +0700

    Update openssh-socket.py

--- sshws/openssh-socket.py
+++ sshws/openssh-socket.py
@@ -12,7 +12,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:88'
-RESPONSE = 'HTTP/1.1 101 WebSocket bye MDX CHANNEL\r\nContent-Length: 104857600000\r\n\r\n'
+RESPONSE = 'HTTP/1.1 101 WebSocket bye Bagoes Vpn\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
     def __init__(self, host, port):
