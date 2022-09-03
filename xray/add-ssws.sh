02ffaa97d73a8e05d78ed602e913bf5079823255
sibeesans <105978585+sibeesans@users.noreply.github.com>
2022-09-03 18:01:08 +0700

    Update add-ssws.sh

--- xray/add-ssws.sh
+++ xray/add-ssws.sh
@@ -4,7 +4,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/kmardhex/swip/perizinan/main/main/allow > /root/tmp
+    curl -sS https://raw.githubusercontent.com/sibeesans/tuunnel-mx/perizinan/main/main/allow > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
@@ -22,7 +22,7 @@ BURIQ () {
 }
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS raw.githubusercontent.com/kmardhex/swip/main/main/allow | grep $MYIP | awk '{print $2}')
+Name=$(curl -sS raw.githubusercontent.com/sibeesans/tuunnel-mx/main/main/allow | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
 
@@ -39,7 +39,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/kmardhex/swip/main/main/allow | awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/sibeesans/tuunnel-mx/main/main/allow | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
