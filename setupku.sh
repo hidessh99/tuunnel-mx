28c9d6a086784ef7e62548dc5f1f8959e547cf10
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-10-13 23:15:23 +0700

    Update setupku.sh

--- setupku.sh
+++ setupku.sh
@@ -24,7 +24,6 @@ ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 sysctl -w net.ipv6.conf.all.disable_ipv6=1 >/dev/null 2>&1
 sysctl -w net.ipv6.conf.default.disable_ipv6=1 >/dev/null 2>&1
 
-echo -e "[ ${green}INFO${NC} ] Preparing the install file"
 apt install git curl -y >/dev/null 2>&1
 apt install python -y >/dev/null 2>&1
 echo -e "[ ${green}INFO${NC} ] Aight good ... installation file is ready"
