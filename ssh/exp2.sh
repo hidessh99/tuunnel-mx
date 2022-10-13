7c3636062157644435223c8c9129ad2de47df8db
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-10-14 01:28:33 +0700

    Create exp2.sh

---
+++ ssh/exp2.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+
+
+MYIP=$(curl -sS ipv4.icanhazip.com)
+
+##----- Auto Remove L2TP
+data=( `cat /var/lib/scrz-prem/data-user-l2tp | grep '^###' | cut -d ' ' -f 2`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^### $user" "/var/lib/scrz-prem/data-user-l2tp" | cut -d ' ' -f 3)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+sed -i "/^### $user $exp/d" "/var/lib/scrz-prem/data-user-l2tp"
+sed -i '/^"'"$user"'" l2tpd/d' /etc/ppp/chap-secrets
+sed -i '/^'"$user"':\$1\$/d' /etc/ipsec.d/passwd
+chmod 600 /etc/ppp/chap-secrets* /etc/ipsec.d/passwd*
+fi
+done
+
+##----- Auto Remove PPTPD
+data=( `cat /var/lib/scrz-prem/data-user-pptp | grep '^###' | cut -d ' ' -f 2`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^### $user" "/var/lib/scrz-prem/data-user-pptp" | cut -d ' ' -f 3)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+sed -i "/^### $user $exp/d" "/var/lib/scrz-prem/data-user-pptp"
+sed -i '/^"'"$user"'" pptpd/d' /etc/ppp/chap-secrets
+chmod 600 /etc/ppp/chap-secrets*
+fi
+done
+
+##----- Auto Remove ShadowSock
+data=( `cat /etc/shadowsocks-libev/akun.conf | grep '^###' | cut -d ' ' -f 2`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^### $user" "/etc/shadowsocks-libev/akun.conf" | cut -d ' ' -f 3)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+sed -i "/^### $user $exp/,/^port_http/d" "/etc/shadowsocks-libev/akun.conf"
+systemctl disable shadowsocks-libev-server@$user-tls.service
+systemctl disable shadowsocks-libev-server@$user-http.service
+systemctl stop shadowsocks-libev-server@$user-tls.service
+systemctl stop shadowsocks-libev-server@$user-http.service
+rm -f "/etc/shadowsocks-libev/$user-tls.json"
+rm -f "/etc/shadowsocks-libev/$user-http.json"
+fi
+done
+
+##----- Auto Remove SSR
+data=( `cat /usr/local/shadowsocksr/akun.conf | grep '^###' | cut -d ' ' -f 2`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^### $user" "/usr/local/shadowsocksr/akun.conf" | cut -d ' ' -f 3)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+sed -i "/^### $user $exp/d" "/usr/local/shadowsocksr/akun.conf"
+cd /usr/local/shadowsocksr
+match_del=$(python mujson_mgr.py -d -u "${user}"|grep -w "delete user")
+cd
+fi
+done
+/etc/init.d/ssrmu restart > /dev/null 2>&1
+
+##----- Auto Remove SSTP
+data=( `cat /var/lib/scrz-prem/data-user-sstp | grep '^###' | cut -d ' ' -f 2`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^### $user" "/var/lib/scrz-prem/data-user-sstp" | cut -d ' ' -f 3)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+sed -i "/^### $user $exp/d" "/var/lib/scrz-prem/data-user-sstp"
+sed -i '/^'"$user"'/d' /home/sstp/sstp_account
+fi
+done
+
+#------TrojanGO
+data=( `cat /etc/trojan-go/akun.conf | grep '^###' | cut -d' ' -f 2` )
+now=$(date +"%Y-%m-%d")
+for user in "${data[@]}"
+do
+exp=( `grep -w "^### $user" "/etc/trojan-go/akun.conf" | awk '{print $3}'` )
+d1=(`date -d "$exp" +%s`)
+d2=(`date -d "$now" +%s`)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+xid=( `grep -E "^### $user" "/etc/trojan-go/akun.conf" | awk '{print $4}'` )
+sed -i '/'$xid'/d' /etc/trojan-go/config.json
+sed -i "/^### $user $exp $xid/d" /etc/trojan-go/akun.conf
+fi
+done
+systemctl restart trojan-go
+
+##----- Auto Remove Wireguard
+data=( `cat /etc/wireguard/wg0.conf | grep '^### Client' | cut -d ' ' -f 3`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^### Client $user" "/etc/wireguard/wg0.conf" | cut -d ' ' -f 4)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+sed -i "/^### Client $user $exp/,/^AllowedIPs/d" /etc/wireguard/wg0.conf
+rm -f "/home/vps/public_html/$user.conf"
+fi
+done
+systemctl restart wg-quick@wg0
+
+##----- Auto Remove Vmess
+data=( `cat /etc/$rekk/config.json | grep '^###' | cut -d ' ' -f 2 | sort | uniq`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^### $user" "/etc/$rekk/config.json" | cut -d ' ' -f 3 | sort | uniq)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
+sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
+rm -f /etc/$rekk/$user-tls.json /etc/$rekk/$user-none.json
+fi
+done
+systemctl restart xray
+
+##------ Auto Remove SSH
+hariini=`date +%d-%m-%Y`
+cat /etc/shadow | cut -d: -f1,8 | sed /:$/d > /tmp/expirelist.txt
+totalaccounts=`cat /tmp/expirelist.txt | wc -l`
+for((i=1; i<=$totalaccounts; i++ ))
+do
+tuserval=`head -n $i /tmp/expirelist.txt | tail -n 1`
+username=`echo $tuserval | cut -f1 -d:`
+userexp=`echo $tuserval | cut -f2 -d:`
+userexpireinseconds=$(( $userexp * 86400 ))
+tglexp=`date -d @$userexpireinseconds`             
+tgl=`echo $tglexp |awk -F" " '{print $3}'`
+while [ ${#tgl} -lt 2 ]
+do
+tgl="0"$tgl
+done
+while [ ${#username} -lt 15 ]
+do
+username=$username" " 
+done
+bulantahun=`echo $tglexp |awk -F" " '{print $2,$6}'`
+todaystime=`date +%s`
+if [ $userexpireinseconds -ge $todaystime ] ;
+then
+:
+else
+userdel --force $username
+fi
+done
