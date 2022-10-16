b8ff7ac068f39ca03d0c69a9dafd0ce364a4c7bb
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-10-16 21:58:33 +0700

    Update belajar-xray.sh

--- xray/belajar-xray.sh
+++ xray/belajar-xray.sh
@@ -1,89 +1,80 @@
 #!/bin/bash
-
-echo - e "
+echo -e "
 "
 date
 echo ""
-domain = $(cat / root / domain)
+domain=$(cat /root/domain)
 sleep 1
-mkdir - p / etc / xray
-echo - e "[ ${green}INFO${NC} ] Checking... "
-apt install iptables iptables - persistent - y
+mkdir -p /etc/xray 
+echo -e "[ ${green}INFO${NC} ] Checking... "
+apt install iptables iptables-persistent -y
 sleep 1
-echo - e "[ ${green}INFO$NC ] Setting ntpdate"
-ntpdate pool.ntp.org
-timedatectl set - ntp true
+echo -e "[ ${green}INFO$NC ] Setting ntpdate"
+ntpdate pool.ntp.org 
+timedatectl set-ntp true
 sleep 1
-echo - e "[ ${green}INFO$NC ] Enable chronyd"
+echo -e "[ ${green}INFO$NC ] Enable chronyd"
 systemctl enable chronyd
 systemctl restart chronyd
 sleep 1
-echo - e "[ ${green}INFO$NC ] Enable chrony"
+echo -e "[ ${green}INFO$NC ] Enable chrony"
 systemctl enable chrony
 systemctl restart chrony
-timedatectl set - timezone Asia / Jakarta
+timedatectl set-timezone Asia/Jakarta
 sleep 1
-echo - e "[ ${green}INFO$NC ] Setting chrony tracking"
-chronyc sourcestats - v
-chronyc tracking - v
-echo - e "[ ${green}INFO$NC ] Setting dll"
+echo -e "[ ${green}INFO$NC ] Setting chrony tracking"
+chronyc sourcestats -v
+chronyc tracking -v
+echo -e "[ ${green}INFO$NC ] Setting dll"
 apt clean all && apt update
-apt install curl socat xz - utils wget apt - transport - https gnupg gnupg2 gnupg1 dnsutils lsb - release - y
-apt install socat cron bash - completion ntpdate - y
+apt install curl socat xz-utils wget apt-transport-https gnupg gnupg2 gnupg1 dnsutils lsb-release -y 
+apt install socat cron bash-completion ntpdate -y
 ntpdate pool.ntp.org
-apt - y install chrony
-apt install zip - y
-apt install curl pwgen openssl netcat cron - y
+apt -y install chrony
+apt install zip -y
+apt install curl pwgen openssl netcat cron -y
 
 
 # install xray
 sleep 1
-echo - e "[ ${green}INFO$NC ] Downloading & Installing xray core"
-domainSock_dir = "/run/xray";
-![-d $domainSock_dir] && mkdir $domainSock_dir
-chown www - data.www - data $domainSock_dir# Make Folder XRay
-mkdir - p /
-    var / log / xray
-mkdir - p / etc / xray
-chown www - data.www - data /
-    var / log / xray
-chmod + x /
-    var / log / xray
-touch /
-    var / log / xray / access.log
-touch /
-    var / log / xray / error.log
-touch /
-    var / log / xray / access2.log
-touch /
-    var / log / xray / error2.log# / / Ambil Xray Core Version Terbaru
-bash - c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)"
-@ install - u www - data--version 1.5 .6
+echo -e "[ ${green}INFO$NC ] Downloading & Installing xray core"
+domainSock_dir="/run/xray";! [ -d $domainSock_dir ] && mkdir  $domainSock_dir
+chown www-data.www-data $domainSock_dir
+# Make Folder XRay
+mkdir -p /var/log/xray
+mkdir -p /etc/xray
+chown www-data.www-data /var/log/xray
+chmod +x /var/log/xray
+touch /var/log/xray/access.log
+touch /var/log/xray/error.log
+touch /var/log/xray/access2.log
+touch /var/log/xray/error2.log
+# / / Ambil Xray Core Version Terbaru
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u www-data --version 1.5.6
 
 
 
 ## crt xray
 systemctl stop nginx
-mkdir / root / .acme.sh
-curl https: //acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
-    chmod + x / root / .acme.sh / acme.sh /
-    root / .acme.sh / acme.sh--upgrade--auto - upgrade /
-    root / .acme.sh / acme.sh--set -
-    default -ca--server letsencrypt /
-    root / .acme.sh / acme.sh--issue - d $domain--standalone - k ec - 256~/.acme.sh/acme.sh--installcert - d $domain--fullchainpath / etc / xray / xray.crt--keypath / etc / xray / xray.key--ecc
+mkdir /root/.acme.sh
+curl https://acme-install.netlify.app/acme.sh -o /root/.acme.sh/acme.sh
+chmod +x /root/.acme.sh/acme.sh
+/root/.acme.sh/acme.sh --upgrade --auto-upgrade
+/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
 
 # nginx renew ssl
-echo - n '#!/bin/bash /
-etc / init.d / nginx stop "/root/.acme.sh" / acme.sh--cron--home "/root/.acme.sh" & > /root/renew_ssl.log /
-    etc / init.d / nginx start /
-    etc / init.d / nginx status ' > /usr/local/bin/ssl_renew.sh
-chmod + x / usr / local / bin / ssl_renew.sh
-if !grep - q 'ssl_renew.sh' /
-    var / spool / cron / crontabs / root;
-then(crontab - l; echo "15 03 */3 * * /usr/local/bin/ssl_renew.sh") | crontab;
-fi
+echo -n '#!/bin/bash
+/etc/init.d/nginx stop
+"/root/.acme.sh"/acme.sh --cron --home "/root/.acme.sh" &> /root/renew_ssl.log
+/etc/init.d/nginx start
+/etc/init.d/nginx status
+' > /usr/local/bin/ssl_renew.sh
+chmod +x /usr/local/bin/ssl_renew.sh
+if ! grep -q 'ssl_renew.sh' /var/spool/cron/crontabs/root;then (crontab -l;echo "15 03 */3 * * /usr/local/bin/ssl_renew.sh") | crontab;fi
 
-mkdir - p / home / vps / public_html
+mkdir -p /home/vps/public_html
 
 #set uuid
 uuid=$(cat /proc/sys/kernel/random/uuid);
