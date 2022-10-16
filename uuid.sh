ad5641293ff951fe483176a4003f705eba036cd3
HIdeSSH <102421846+hidessh99@users.noreply.github.com>
2022-10-16 13:39:26 +0700

    Update uuid.sh

--- uuid.sh
+++ uuid.sh
@@ -4,6 +4,4 @@ uuid = $(cat/proc / sys / kernel / random / uuid)# xray config
 uuid2="s/xxxxx/$uuid/g";
 
 sed -i $uuid2 /etc/xray/vmess-ws.json 
-
-wget -O /etc/xray/vmess-grpc.json https://raw.githubusercontent.com/hidessh99/tuunnel-mx/main/hide-xray/vmess-grpc.json && chmod +x /etc/xray/vmess-grpc.json
 sed -i $uuid2 /etc/xray/vmess-grpc.json
